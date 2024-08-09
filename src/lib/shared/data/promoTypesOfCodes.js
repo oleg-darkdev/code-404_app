c615eebb90a7db45857f7148c46e9d37796a8b38
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:55:51 +0300

    alfa v.

---
+++ src/lib/shared/data/promoTypesOfCodes.js
@@ -0,0 +1,27 @@
+const promoTypesOfCodes = {
+  title: 'Codes that you will learn.',
+  link: 'codes',
+  promo: [
+    {
+      title: 'HTTP codes',
+      complexity: 'basic',
+      code: 'http',
+      img: '',
+    },
+    {
+      title: 'SMTP codes',
+      complexity: 'basic',
+      code: 'smtp',
+      img: '',
+    },
+      {
+      title: 'FTP codes',
+      complexity: 'basic',
+      code: 'ftp',
+      img: '',
+    }
+  ]
+};
+
+
+export default promoTypesOfCodes;
---
+++ src/lib/shared/data/sponsors.js
@@ -0,0 +1,37 @@
+const sponsors = [
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/apache.svg'
+  },
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/aws.svg'
+  },
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/cloudflare.svg'
+  },
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/iss.svg'
+  },
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/nginx.svg'
+  },
+  {
+    title: '',
+    link: '/',
+    logo: '/images/sponsors/shopify.svg'
+  },
+
+
+]
+
+
+export default sponsors;

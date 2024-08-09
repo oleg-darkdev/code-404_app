f05769aae198f1d975d5a7e196b5218326b0e905
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:51:35 +0300

    init http NGINX codes

---
+++ src/lib/shared/data/codes/http/unofficialCodes/nginx/clientClosedRequest_499.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Client Closed Request',
+  img: '/images/codes/http/nginx/deck_nginx_499.svg',
+  code: 499,
+  complexity: 'Nginx',
+
+  shortDesc: 'Used when the client has closed the request before the server could send a response.',
+  desc: ['']
+};
+
+export default data;
---
+++ src/lib/shared/data/codes/http/unofficialCodes/nginx/requestHeaderTooLarge_494.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Request header too large',
+  img: '/images/codes/http/nginx/deck_nginx_494.svg',
+  code: 494,
+  complexity: 'Nginx',
+
+  shortDesc: 'Client sent too large request or too long header line.',
+  desc: ['', '']
+};
+
+export default data;

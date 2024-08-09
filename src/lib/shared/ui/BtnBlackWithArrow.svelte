b3fb0ceaa1896da3d9ecb3d0287603e08ea95aad
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:56:12 +0300

    stable v.

---
+++ src/lib/shared/ui/BtnBlackWithArrow.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let link, text;
+</script>
+
+<a href="{link}" class="std-button black w-inline-block">
+  <div class="normal-text button-text">{text}</div>
+  <img src="images/arrow.svg" loading="lazy" alt="Right arrow icon" class="button-arrow white">
+</a>
+
+
+<style>
+
+</style>

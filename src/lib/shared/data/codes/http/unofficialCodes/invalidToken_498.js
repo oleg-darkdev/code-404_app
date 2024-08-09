3fd628d3f478a7e52eb234615a5a397808e70b3c
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:51:24 +0300

    init http unnoficial codes

---
+++ src/lib/shared/data/codes/http/unofficialCodes/invalidToken_498.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Invalid Token (Esri)',
+  img: '/images/codes/http/unofficial/desk_depr_498.svg',
+  code: 498,
+  complexity: 'Esri',
+
+  shortDesc: 'Returned by ArcGIS for Server.',
+  desc: ['Code 498 indicates an expired or otherwise invalid token.']
+};
+
+export default data;

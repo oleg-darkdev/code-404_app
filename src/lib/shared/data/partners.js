5684632f54b7a0548351e2d190092767fbe92edc
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:53:41 +0300

    alfa v.

---
+++ src/lib/shared/data/partners.js
@@ -0,0 +1,17 @@
+const partners = [
+  {
+    title: 'DarkDev.Foundation',
+    link: '/'
+  },
+  {
+    title: '44Games',
+    link: '/'
+  },
+  {
+    title: 'Gdansk FabLab',
+    link: '/'
+  },
+];
+
+
+export default partners;

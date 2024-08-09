5b468591b38892fc8d54ed52268d24e4010a6729
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:58:06 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/free-2.svelte
@@ -0,0 +1,149 @@
+
+    <div class="cart-modal">
+      <div scroll="enable" data-w-id="255d5b0b-33cf-f30e-7ac5-00733b1141dc" class="cart-blend"></div>
+      <div id="cart-modal" class="cart-wrapper">
+        <div class="cart-inner-wrapper">
+          <div class="big-heading has__all-caps cc-cart-heading">Warenkorb</div>
+          <div id="cart-item-wrapper" class="cart-item-wrapper">
+            <div id="cart-empty-wrapper" class="cart-empty-wrapper">
+              <div class="cart-empty-text">Dein Korb ist leer. Wie wärs denn hiermit? :)</div>
+              <div class="cart-suggestion-wrapper w-dyn-list">
+                <div role="list" class="cart-suggestion-list w-dyn-items">
+                  <div role="listitem" class="cart-suggestion-item w-dyn-item">
+                    <a href="/produkte/achtzig-zwanzig" class="link-block-product cc-cart-suggestion w-inline-block">
+                      <div class="product-meta-wrapper">
+                        <div class="product-heading-wrapper">
+                          <h3 class="cart-item-overview-heading">ACHTZIG ZWANZIG dark roast</h3>
+                          <div class="organic-logo w-condition-invisible">
+                            <img src="images/613c8a9ba868284649a34016_EU-Organic-Logo.svg" loading="lazy" alt="EU-Bio-Logo. Vorverpackte Bio-Lebensmittel, aus der EU, welche die strengen Normen der EU-Rechtsvorschriften für den ökologischen Landbau erfüllen, müssen seit dem 1. Juli 2010 verpflichtend mit dem EU-Bio-Logo gekennzeichnet werden." class="organic-logo-eu">
+                          </div>
+                        </div>
+                        <div class="product-overview-price-wrapper">
+                          <div class="product-overview-price-holder main-price">
+                            <div price="ACHTZIG ZWANZIG dark roast" class="product-overview-price">10.00</div>
+                            <div class="product-overview-price">€</div>
+                          </div>
+                          <div class="product-overview-price-holder sold-out w-condition-invisible">
+                            <div fs-cmssort-field="price" class="product-overview-price">sold out</div>
+                          </div>
+                          <div class="product-overview-price-holder off-price w-condition-invisible">
+                            <div price="ACHTZIG ZWANZIG dark roast" class="product-overview-price off-price w-dyn-bind-empty"></div>
+                            <div class="product-overview-price">€</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="product-thumbnail-wrapper">
+                        <img src="images/641329557df473d965c591d8_AchtzigZwanzig-KROeMKaffeeroestereiMagdeburg_67eecd8d-a7a7-4b9d-a40e-074287d41ea9.jpeg" loading="lazy" alt="ACHTZIG ZWANZIG dark roast" sizes="100vw" srcset="images/641329557df473d965c591d8_AchtzigZwanzig-KROeMKaffeeroestereiMagdeburg_67eecd8d-a7a7-4b9d-a40e-074287d41ea9-p-500.jpeg 500w, images/641329557df473d965c591d8_AchtzigZwanzig-KROeMKaffeeroestereiMagdeburg_67eecd8d-a7a7-4b9d-a40e-074287d41ea9-p-800.jpeg 800w, images/641329557df473d965c591d8_AchtzigZwanzig-KROeMKaffeeroestereiMagdeburg_67eecd8d-a7a7-4b9d-a40e-074287d41ea9-p-1080.jpeg 1080w, images/641329557df473d965c591d8_AchtzigZwanzig-KROeMKaffeeroestereiMagdeburg_67eecd8d-a7a7-4b9d-a40e-074287d41ea9.jpeg 1200w" class="product-image cc-landingpage">
+                      </div>
+                    </a>
+                  </div>
+                  <div role="listitem" class="cart-suggestion-item w-dyn-item">
+                    <a href="/produkte/athiopien-light-roast" class="link-block-product cc-cart-suggestion w-inline-block">
+                      <div class="product-meta-wrapper">
+                        <div class="product-heading-wrapper">
+                          <h3 class="cart-item-overview-heading">ÄTHIOPIEN light roast</h3>
+                          <div class="organic-logo w-condition-invisible">
+                            <img src="images/613c8a9ba868284649a34016_EU-Organic-Logo.svg" loading="lazy" alt="EU-Bio-Logo. Vorverpackte Bio-Lebensmittel, aus der EU, welche die strengen Normen der EU-Rechtsvorschriften für den ökologischen Landbau erfüllen, müssen seit dem 1. Juli 2010 verpflichtend mit dem EU-Bio-Logo gekennzeichnet werden." class="organic-logo-eu">
+                          </div>
+                        </div>
+                        <div class="product-overview-price-wrapper">
+                          <div class="product-overview-price-holder main-price">
+                            <div price="ÄTHIOPIEN light roast" class="product-overview-price">12.00</div>
+                            <div class="product-overview-price">€</div>
+                          </div>
+                          <div class="product-overview-price-holder sold-out w-condition-invisible">
+                            <div fs-cmssort-field="price" class="product-overview-price">sold out</div>
+                          </div>
+                          <div class="product-overview-price-holder off-price w-condition-invisible">
+                            <div price="ÄTHIOPIEN light roast" class="product-overview-price off-price w-dyn-bind-empty"></div>
+                            <div class="product-overview-price">€</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="product-thumbnail-wrapper">
+                        <img src="images/6386341915e596443954002a_AethiopienLaenderkaffee_KROeMKaffeeroestereiMagdeburg.jpeg" loading="lazy" alt="ÄTHIOPIEN light roast" class="product-image cc-landingpage">
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="cart-item-loading-spinner" class="cart-item-loading-spinner">
+              <div class="loading-spinner" data-w-id="7530af3d-e416-203f-25d3-3207bbc9251b" data-animation-type="lottie" data-src="https://cdn.prod.website-files.com/610cf33140aaf753c6f8f7bb/62695a031f1563936b525a72_lf30_editor_3sebc4ky.json" data-loop="1" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="3.16" data-duration="0"></div>
+            </div>
+          </div>
+          <div class="cart-summary-wrapper">
+            <div class="cart-price-wrapper">
+              <div class="cart-price">Zwischensumme:</div>
+              <div id="cart-price" class="cart-price">00,00€</div>
+            </div>
+            <div class="checkout-info">Versand und Rabattcodes werden beim Checkout hinzugefügt.</div>
+            <div id="cart-checkout-button" class="std-button is__black is__bigger is__100 is__not-active">Zur Kasse</div>
+          </div>
+          <div scroll="enable" data-w-id="255d5b0b-33cf-f30e-7ac5-00733b1141e8" class="close-cart-modal-button">
+            <div class="x-stroke rotated"></div>
+            <div class="x-stroke"></div>
+          </div>
+          <div id="cart-preloader" class="cart-preloader">
+            <div class="div-block-32">
+              <div data-w-id="d3da09da-f961-afd7-dc20-602f410e8f5d" data-animation-type="lottie" data-src="https://cdn.prod.website-files.com/610cf33140aaf753c6f8f7bb/61965ab806482f517e990003_herdkanne%20-%20login.json" data-loop="1" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="5.005004801146985" data-duration="0"></div>
+              <div class="normal-text is__bold">Gleich gehts weiter zur Kasse</div>
+            </div>
+          </div>
+          <div class="cart-maintence-block">
+            <div class="margin-bottom margin-small">
+              <div class="semi-heading">Wir sind gleich zurück 🚧</div>
+            </div>
+            <div class="normal-text">Hey du! Sorry! Aktuell führen wir Wartungsarbeiten an der Seite durch. Schau bitte später nochmal vorbei wenn wir durch sind :-). Vielen Dank für dein Verständnis! KRÖM</div>
+          </div>
+        </div>
+      </div>
+      <div class="cart-background"></div>
+    </div>
+    <div class="section cc-newsletter-page">
+      <div class="container">
+        <div class="layout-newsletter-page">
+          <div class="newsletter-content">
+            <div class="padding-vertical padding-huge">
+              <h1 class="heading-newsletter">News die du riechen kannst!</h1>
+              <h2 class="text_newsletter">Erfahre als erstes von neuen Kreationen, Rezepten sowie Aktionen und erhalte außerdem <strong>10% Rabatt</strong> auf deinen ersten Einkauf bei uns! </h2>
+              <div class="newsletter-page-form w-form">
+                <form id="wf-form-Newsletter-Page" name="wf-form-Newsletter-Page" data-name="Newsletter Page" method="get" class="form-2" data-wf-page-id="6409b7a03d21182e7b3b3ac3" data-wf-element-id="34e29a49-9641-1449-ad5c-1abb83b85227">
+                  <div data-w-id="96cd4bb5-eaa8-3c65-c8b7-f30e5ca810f8" class="newsletter-page-input-wrapper">
+                    <input class="newsletter-page-form-field w-input" autofocus="true" maxlength="256" name="name" data-name="Name" placeholder="E-Mail Adresse" newsletter="input-field" type="email" id="name" required="">
+                    <input type="submit" data-wait="Momentchen bitte..." newsletter="submit" class="newsletter-page-submit-button w-button" value="Anmelden">
+                    <div class="box-shadow-on-mobile"></div>
+                  </div>
+                  <label id="newsletter-consent" newsletter="consent" style="display:none;height:0px" class="w-checkbox newsletter-consent">
+                    <div class="w-checkbox-input w-checkbox-input--inputType-custom newsletter-consent-checkbox"></div>
+                    <input id="GDPR-Consent-Newsletter-Page" type="checkbox" name="GDPR-Consent-Newsletter-Page" data-name="GDPR Consent Newsletter Page" required="" style="opacity:0;position:absolute;z-index:-1">
+                    <span class="smalll-text w-form-label" for="GDPR-Consent-Newsletter-Page">Die von Ihnen angegebenen personenbezogenen Daten werden zum Zweck der Zurverfügungstellung des Newsletters aufgrund Ihrer erteilten Einwilligung gemäß Art. 6 Abs. 1 UAbs. 1 lit. a) DSGVO verarbeitet.“ Weitere Informationen entnehmen Sie der <a href="/datenschutz">Datenschutzerklärung</a>. Sie können den Newsletter jederzeit widerrufen. </span>
+                  </label>
+                </form>
+                <div class="success-message-2 w-form-done">
+                  <div>Danke! Bitte klicke auf den Link in der E-Mail die wir dir zugeschickt haben, um deine E-Mail Adresse zu bestätigen. 🤝</div>
+                </div>
+                <div class="w-form-fail">
+                  <div>Oops! Something went wrong while submitting the form.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="newsletter-page-dividier-wrapper">
+            <div class="divider is-vertical"></div>
+          </div>
+          <div class="newsletter-content image-side">
+            <div class="padding-vertical">
+              <div class="newsletter-page-image-wrapper">
+                <img src="images/640a05c9746c467846c2bf8b_Envelpoe%20Newsletter.svg" loading="lazy" data-w-id="960faad0-7995-4e45-16f3-ab5ea75dc888" alt="" class="newsletter-page-image _4">
+                <img src="images/640a05c9746c467846c2bf8b_Envelpoe%20Newsletter.svg" loading="lazy" data-w-id="59923235-0647-c79e-e3b9-2e6611957251" alt="" class="newsletter-page-image _3">
+                <img src="images/640a05c9746c467846c2bf8b_Envelpoe%20Newsletter.svg" loading="lazy" data-w-id="63e03c10-722f-8729-d77b-24e3cb5ef8dd" alt="" class="newsletter-page-image _2">
+                <img src="images/640a05c9746c467846c2bf8b_Envelpoe%20Newsletter.svg" loading="lazy" alt="" class="newsletter-page-image _1">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+

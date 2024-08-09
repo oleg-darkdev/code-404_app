f878b70ccf1af35914e4063977446e510680a710
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:48:20 +0300

    init http 1xx codes

---
+++ src/lib/shared/data/codes/http/100/processing_102.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Processing',
+  img: '/images/codes/http/100/desc_102.svg',
+  code: 101,
+    complexity: 'base',
+
+  shortDesc: 'A WebDAV request may contain many sub-requests involving file operations, requiring a long time to complete the request.',
+  desc: ['This code indicates that the server has received and is processing the request, but no response is available yet. This prevents the client from timing out and assuming the request was lost.', 'The status code is deprecated.']
+};
+
+export default data;
---
+++ src/lib/shared/data/codes/http/100/switching_101.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Switching Protocols',
+    img: '/images/codes/http/100/desc_101.svg',
+  code: 101,
+    complexity: 'base',
+
+  shortDesc: 'The requester has asked the server to switch protocols and the server has agreed to do so.',
+  desc: ['', '']
+};
+
+export default data;

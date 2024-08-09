699b60e00d0ebb8bc90cae353884f4bbbc9dd7dc
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:51:41 +0300

    init http unnoficial codes

---
+++ src/lib/shared/data/codes/http/unofficialCodes/pageExpired_419.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Page Expired (Laravel Framework)',
+  img: '/images/codes/http/unofficial/desk_depr_419.svg',
+  code: 419,
+  complexity: 'Laravel',
+
+  shortDesc: 'Used by the Laravel Framework when a CSRF Token is missing or expired',
+  desc: ['']
+};
+
+export default data;

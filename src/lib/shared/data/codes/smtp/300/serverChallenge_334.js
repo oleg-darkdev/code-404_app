7106a9e0177fa9f16262bfc76cb67257ec056ad9
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:52:39 +0300

    init smtp 3xx codes

---
+++ src/lib/shared/data/codes/smtp/300/serverChallenge_334.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Server challenge - the text part contains the Base64-encoded challenge.',
+  img: '/images/codes/smtp/desc_334.svg',
+  code: 334,
+  complexity: 'base',
+
+  shortDesc: '',
+  desc: ['', '']
+};
+
+export default data;
---
+++ src/lib/shared/data/codes/smtp/300/startMailInput_354.js
@@ -0,0 +1,11 @@
+const data = {
+  title: 'Start mail input.',
+  img: '/images/codes/smtp/desc_354.svg',
+  code: 354,
+  complexity: 'base',
+
+  shortDesc: '',
+  desc: ['', '']
+};
+
+export default data;

86332a86324fafc7d67400d9673d3f545cde4c6b
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:53:34 +0300

    alfa v.

---
+++ src/lib/shared/data/navigation.js
@@ -0,0 +1,33 @@
+const navigation = [
+  {
+    title: 'About',
+    link: '#about'
+  },
+  {
+    title: 'Prices',
+    link: '#prices'
+  },
+  {
+    title: 'F.A.Q.',
+    link: '#faq'
+  },
+  {
+    title: 'Contact',
+    link: '#contact'
+  },
+  {
+    title: 'HTTP',
+    link: '#http'
+  },
+  {
+    title: 'SMTP',
+    link: '#smtp'
+  },
+  {
+    title: 'FTP',
+    link: '#ftp' 
+  },
+]
+
+
+export default navigation;

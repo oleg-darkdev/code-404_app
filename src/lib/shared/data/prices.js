abe4877f86087ccce58098b0bec2d502383d48a9
DarkDev <darkdev_brand@riseup.net>
2024-08-09 22:53:44 +0300

    alfa v.

---
+++ src/lib/shared/data/prices.js
@@ -0,0 +1,53 @@
+const prices = {
+    basic: {
+      title: 'Basic',
+      subTitle: 'lorem impsum',
+      notify: 'lorem',
+      price: '26.00',
+      shortDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      complect: [
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+      ]
+    },
+    hacker: {
+      title: 'Hacker',
+      subTitle: 'lorem impsum',
+      notify: 'lorem',
+      price: '46.00',
+      shortDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      complect: [
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+      ]
+    },
+    geek: {
+      title: 'Expert',
+      subTitle: 'lorem impsum',
+      notify: 'lorem',
+      price: '36.00',
+      shortDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      complect: [
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply',
+      ]
+    },
+}
+
+
+export default prices;

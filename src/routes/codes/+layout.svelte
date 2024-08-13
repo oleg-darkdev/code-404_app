8ca5a2d12316f0f3bd497883c98883cb7594b85d
DarkDev <darkdev_brand@riseup.net>
2024-08-13 03:51:30 +0300

    alfa v. of section structure

---
+++ src/routes/codes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {  Faq, Footer, MemoryCards,} from '$lib/widgets';
+ //  import {  } from '$lib/entities';
+</script>
+
+
+<svelte:head>
+  <title>CODE 404</title>
+</svelte:head>
+
+
+<MemoryCards />
+
+<slot />
+
+<Faq />
+
+<Footer />

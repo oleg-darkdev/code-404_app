15a10c4d1266d38b6d484965ce5f848c18a03caa
DarkDev <darkdev_brand@riseup.net>
2024-08-13 03:52:21 +0300

    intial

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/ eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function sort_by_key(array: any[], key: string) {
   return array.sort(function (a, b) {
     const x = a[key]; const y = b[key];
